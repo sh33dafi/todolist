781c5b337d66a635d4058b267deb5d2a99b78a63
Yannick Houbrix <yannickhoubrix@Yannicks-MacBook-Pro.local>
2017-04-23 12:05:41 +0200

    Master

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,21 +1,12 @@
-import {ChangeDetectionStrategy, Component} from "@angular/core";
-import {TodoService} from "../../services/todo.service";
+import {Component} from "@angular/core";
 
 @Component({
   selector: "app-root",
   styleUrls: ["./app.component.scss"],
-  providers: [TodoService],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <h1>Todos</h1>
+    <img src="https://angular.io/resources/images/logos/angular/angular.png" alt="Angular" />
   `
 })
 export class AppComponent {
-
-  public todos$;
-
-  constructor(private todoService: TodoService) {
-    this.todos$ = this.todoService.todos$;
-  }
 
 }
